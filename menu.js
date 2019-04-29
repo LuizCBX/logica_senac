bf68b828e40f1641c9ea84fadc35fa01d17391e0
LuizCBX <43183856+LuizCBX@users.noreply.github.com>
2019-04-29 09:27:15 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,54 @@
+/**
+ * @author Geovana
+ */
+//let onde = window.location.pathname;
+//alert (onde)
+function abrirSite() {
+    let Entrada
+    Entrada = parseInt(frmAbrir.txtEntrada.value)
+    switch (Entrada) {
+        case 1:
+            window.location = "/C:/Users/Luiz/Desktop/Projeto_logica/areaCirculo/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 2:
+            window.location = "/C:/Users/Luiz/Desktop/Projeto_logica/calcImc/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 3:
+            window.location = "/C:/Users/Luiz/Desktop/Projeto_logica/dolar/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 4:
+            window.location = "/C:/Users/Luiz/Desktop/Projeto_logica/conversorCelsius/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 5:
+            window.location = "/C:/Users/Luiz/Desktop/Projeto_logica/churrasCalculadora/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 6:
+            window.location = "/C:/Users/Luiz/Desktop/Projeto_logica/valorServico/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 7:
+            window.location = "/C:/Users/Luiz/Desktop/Projeto_logica/jokenpo/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 8:
+            window.location = "/C:/Users/Luiz/Desktop/Projeto_logica/tabuadaWhile/index.html";
+            break
+    }
+    switch (Entrada) {
+        case 9:
+            window.location = "/C:/Users/Luiz/Desktop/Projeto_logica/salarioBonus/index.html";
+            break
+    }
+}
